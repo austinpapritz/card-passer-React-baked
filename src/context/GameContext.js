7ce646db87a9a4ddcf368ad59eda0e2e976d835f
Austin Papritz <austin@papritz.dev>
2023-01-25 15:34:07 -0800

    moved playerhand and to/from states, passCard() and findCardIndex()  to gamecontext

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,11 +1,53 @@
-import { useState } from 'react';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   //useStates
-  return <GameContext.Provider value={{}}>{children}</GameContext.Provider>;
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+
+  function findCardIndex(value, suit, cards) {
+    return cards.findIndex((card) => card.value === value && card.suit === suit);
+  }
+
+  function passCard(card) {
+    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+
+    // arrays start at zero, but our players start at 1 :shrug:
+    const toHand = playerHands[to - 1] || deck;
+    const fromHand = playerHands[from - 1] || deck;
+
+    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+
+    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+
+    toHand.push(cardToMove);
+
+    toSetFunction([...toHand]);
+    fromSetFunction([...fromHand]);
+
+    setSelectedCard(null);
+  }
+
+  return (
+    <GameContext.Provider
+      value={{
+        playerOneHand,
+        setPlayerOneHand,
+        playerTwoHand,
+        setPlayerTwoHand,
+        playerThreeHand,
+        setPlayerThreeHand,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 export { GameProvider, GameContext };
