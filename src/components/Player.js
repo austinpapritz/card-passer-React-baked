16b94fb49896d61b9d7df79413774a8f098750d8
Austin Papritz <austin@papritz.dev>
2023-01-25 17:32:15 -0800

    trimmed up Player.js to only import to and steTo from GameContext

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,20 +3,14 @@ import { GameContext } from '../context/GameContext.js';
 import CardList from './CardList';
 
 export default function Player({ player, hand }) {
-  const { setTo, setFrom, setSelectedCard, selectedCard, to } = useContext(GameContext);
+  const { to, setTo } = useContext(GameContext);
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
       onClick={() => setTo(player)}
     >
       <p>Player {player}</p>
-      <CardList
-        player={player}
-        cards={hand}
-        setFrom={setFrom}
-        selectedCard={selectedCard}
-        setSelectedCard={setSelectedCard}
-      />
+      <CardList player={player} cards={hand} />
     </div>
   );
 }
